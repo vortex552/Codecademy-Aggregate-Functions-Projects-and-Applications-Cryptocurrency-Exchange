c04af779f7e38cc7dba37fa9d3162136d482bd82
vortex552 <nielsvwinden@gmail.com>
2021-08-17 14:58:51 +0200

    Create Nested_Loops.py

---
+++ Nested_Loops.py
@@ -0,0 +1,10 @@
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+
+scoops_sold = 0
+
+for location in sales_data:
+  print(location)
+  for sales in location:
+    scoops_sold += sales
+    
+print(scoops_sold)
