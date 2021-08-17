861b113ccb288d2bc6d8309e63b6a56a3be01599
vortex552 <nielsvwinden@gmail.com>
2021-08-17 14:28:11 +0200

    Create While_Statement_Loops:_Lists.py

---
+++ While_Statement_Loops:_Lists.py
@@ -0,0 +1,8 @@
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+#Your code below: 
+length = len(python_topics)
+index = 0
+while index < length:
+  print("I am learning about " + str(python_topics[index]))
+  index += 1
