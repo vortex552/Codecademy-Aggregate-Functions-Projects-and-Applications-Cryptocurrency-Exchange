1bd26ddc48b5970a673f5d1c150f7ef21326f85e
vortex552 <nielsvwinden@gmail.com>
2021-08-29 21:54:35 +0200

    carly's clippers

---
+++ carlysclippers_under302.py
@@ -0,0 +1,24 @@
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+new_prices = [price - 5 for price in prices]
+
+cuts_under_30 = 0
+
+cuts_under_30 = [hairstyles[hairstyle] for hairstyle in range(len(hairstyles)) if new_prices[hairstyle] < 30]
+
+print(cuts_under_30)
+
+#the code below produces the same output as the above
+under_30_hairstyles = list
+under_30_hairstyles = []
+i = 0
+while i < len(hairstyles):
+    if (new_prices[i] < 30):
+        under_30_hairstyles.append(hairstyles[i])
+        i = i + 1
+    else:
+        i = i + 1
+
+print(under_30_hairstyles)
